f375e1e7631768ec76c3fb6a33d8e63041ffa5be
Om Kathe <omkathe26@gmail.com>
2023-01-18 23:11:35 +0530

    add-files

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const num: number = 55;
+console.log(num);
+
+function print(arg: any){
+    console.log(arg);
+}
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    module:{
+        rules:[
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'ts-loader'
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    }
+}
